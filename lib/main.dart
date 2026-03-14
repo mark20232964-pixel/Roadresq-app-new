a4b2b647dd692eeb65950c180e6d5156d52de517
Sanjana Karannagoda <sanjanakarannagoda12@gmail.com>
2026-03-14 12:07:50 +0530

    yaml errors fixed

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:roadresq/screens/common/welcome_screen.dart';
+<<<<<<< HEAD
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+=======
+
+void main() {
+>>>>>>> b84fd283355c2ca1745810ca10bfff9bb0ac93db
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
+  runApp(const RoadResQApp());
+}
+
+class RoadResQApp extends StatelessWidget {
+  const RoadResQApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RoadResQ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF0D1117),
+        useMaterial3: true,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
